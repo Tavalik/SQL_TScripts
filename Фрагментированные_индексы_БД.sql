f8b6cc7d9a4a04ba2cdcf8db9f749d3c1036b279
Tavalik <tavalik@mail.ru>
2017-05-18 16:49:09 +0600

    Добавил скрпит

---
+++ Фрагментированные_индексы_БД.sql
@@ -0,0 +1,30 @@
+﻿-------------------------------------------
+-- Показывает фрагментированные индексы для указанной базы данных
+-- Автор: Онянов Виталий (Tavalik.ru)
+-- Версия от 18.05.2017
+-------------------------------------------
+
+-- НАСТРАИВАЕМЫЕ ПЕРЕМЕННЫЕ
+-- База данных для анализа
+USE fto_test_01 
+
+-- Отбираем объекты, которые:
+--	 являются индексами (index_id > 0)
+--   фрагментация которых более 5% 
+--   количество страниц в индексе более 128
+SELECT
+	OBJECT_NAME(object_id) AS TableName, 
+	object_id,
+    index_id,
+    partition_number,
+	page_count, 
+	partition_number, 
+	index_type_desc,
+    avg_fragmentation_in_percent
+FROM sys.dm_db_index_physical_stats (DB_ID(), NULL, NULL , NULL, 'LIMITED')
+WHERE index_id > 0 
+	AND avg_fragmentation_in_percent > 5.0
+	AND page_count > 128
+ORDER BY avg_fragmentation_in_percent DESC
+
+GO
