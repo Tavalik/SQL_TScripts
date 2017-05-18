06e30fffafdae24fffdf796fc176f61e5a1134e4
Tavalik <tavalik@mail.ru>
2017-05-18 14:27:02 +0600

    Изменил кодировку файла

--- Восстановление_БД1_из_имеющихся_копий_БД2.sql
+++ Восстановление_БД1_из_имеющихся_копий_БД2.sql
@@ -1,30 +1,30 @@
 ﻿-------------------------------------------
--- ������ �������������� ���� ���� ������ �� ��������� ����� ������ ���� �� ��������� ���� ("������������" ����). 
---	�������� ������:
---		1. ����������� ���������������� ������� ������ ��������� ����� ����-��������� �� ��������� ����
---		2. ����������������� ����-���������� �� ��������� ������������������ ������
---		3. ����-���������� ����������� � ����������� �����
---		4. ����-���������� ����������� � ������� ������ ��������������
---		5. ����-���������� ���������
---		6. ������������ ����������� ��������� � ���������� ������ � �������������� ������������ ��������� �������
--- �����: ������ ������� (Tavalik.ru)
--- ������ �� 18.05.2017
+-- Скрипт восстанавливет одну базу данных из резервной копии другой базы на указанную дату ("перезаливает" базу). 
+--	Алгоритм работы:
+--		1. Подбирается последовательная цепочка файлов резервной копии базы-источника на указанную дату
+--		2. Восстановливается база-назначения из полученой последовательности файлов
+--		3. База-назначения переводится в оперативный режим
+--		4. База-назначения переводится в простую модель восстановления
+--		5. База-назначения сжимается
+--		6. Отправляется электронное сообщение о результате работы с использованием настроенного почтового профиля
+-- Автор: Онянов Виталий (Tavalik.ru)
+-- Версия от 18.05.2017
 -------------------------------------------
 
--- ������������� ����������
--- ���� ������ ����������
+-- НАСТРАИВАЕМЫЕ ПЕРЕМЕННЫЕ
+-- База данных назначения
 DECLARE @DBName_To as nvarchar(40) = 'TestBase'
--- ���� ������ ��������						
+-- База данных источник						
 DECLARE @DBName_From as nvarchar(40) = 'WorkBase'
--- ����, �� �������� ���������� ������� ������ ��������� �����, � ������� '20160315 12:00:00'							
+-- Дата, на котороую собирается цепочка файлов резервных копий, в формате '20160315 12:00:00'							
 DECLARE @BackupTime as datetime = GETDATE()
--- ��� ��������� �������, ��� �������� ���������� �����									
-DECLARE @profile_name as nvarchar(100) = '���_��������'
--- ���������� ��������� ����������� �����, ����������� ������ ";"				
+-- Имя почтового профиля, для отправки электонной почты									
+DECLARE @profile_name as nvarchar(100) = 'ОсновнойПрофиль'
+-- Получатели сообщений электронной почты, разделенные знаком ";"				
 DECLARE @recipients as nvarchar(500) = 'admin@mydomen.com'
 
 -------------------------------------------
--- ��������� ����������	
+-- СЛУЖЕБНЫЕ ПЕРЕМЕННЫЕ	
 DECLARE @SQLString NVARCHAR(4000)
 DECLARE @backupfile NVARCHAR(500)
 DECLARE @physicalName NVARCHAR(500), @logicalName NVARCHAR(500)
@@ -33,16 +33,16 @@ DECLARE @subject as NVARCHAR(100)
 DECLARE @finalmassage as NVARCHAR(1000)
 
 -------------------------------------------
--- ���� �������
+-- ТЕЛО СКРИПТА
 use master
 
--- ������ ��������� �������, ���� ����� ��� ����
+-- Удалим временные таблицы, если вдруг они есть
 IF OBJECT_ID('tempdb.dbo.#BackupFiles') IS NOT NULL DROP TABLE #BackupFiles
 IF OBJECT_ID('tempdb.dbo.#FullBackup') IS NOT NULL DROP TABLE #FullBackup
 IF OBJECT_ID('tempdb.dbo.#DiffBackup') IS NOT NULL DROP TABLE DiffBackup
 IF OBJECT_ID('tempdb.dbo.#LogBackup') IS NOT NULL DROP TABLE LogBackup
 
--- ������� ������ � ���� ��������� ������ �������
+-- Соберем данные о всех сдаланных раннее бэкапах
 SELECT
 	backupset.backup_start_date,
 	backupset.backup_set_uuid,
@@ -55,11 +55,11 @@ FROM msdb.dbo.backupset AS backupset
 	ON backupset.media_set_id = backupmediafamily.media_set_id
 WHERE backupset.database_name = @DBName_From 
 	and backupset.backup_start_date < @BackupTime
-	--and backupset.is_copy_only = 0 -- ���� "������ ��������� �����������"
+	--and backupset.is_copy_only = 0 -- флаг "Только резервное копирование"
 ORDER BY 
 	backupset.backup_start_date DESC
 
--- ������ ��������� ������ �����
+-- Найдем последний полный бэкап
 SELECT TOP 1
 	BackupFiles.backup_start_date,
 	BackupFiles.physical_device_name,
@@ -69,7 +69,7 @@ FROM #BackupFiles AS BackupFiles
 WHERE btype = 'D'
 ORDER BY backup_start_date DESC
 
--- ������ ��������� ���������� �����
+-- Найдем последний разностный бэкап
 SELECT TOP 1
 	BackupFiles.backup_start_date,
 	BackupFiles.physical_device_name
@@ -80,7 +80,7 @@ FROM #BackupFiles AS BackupFiles
 WHERE BackupFiles.btype = 'I'
 ORDER BY BackupFiles.backup_start_date DESC
 
--- ������� ������ �������� ����������
+-- Соберем бэкапы журналов транзакций
 SELECT
 	BackupFiles.backup_start_date,
 	BackupFiles.physical_device_name
@@ -102,7 +102,7 @@ FROM #BackupFiles AS BackupFiles
 WHERE BackupFiles.btype = 'L'
 ORDER BY BackupFiles.backup_start_date DESC
 
--- ���������� ���� �� ����������� ���� ���� ������
+-- Инициируем цикл по объединению всех трех таблиц
 DECLARE bkf CURSOR LOCAL FAST_FORWARD FOR 
 (
 	SELECT
@@ -118,31 +118,31 @@ DECLARE bkf CURSOR LOCAL FAST_FORWARD FOR
 	FROM #LogBackup
 );
 
--- ������ �����
+-- Начало цикла
 OPEN bkf;
 
--- ��������� ������ ������� �����, �� ����� ���� ������ ������ ��������� �����
+-- Прочитаем первый элемент цикла, им может быть только полная резервная копия
 FETCH bkf INTO @backupfile;
 IF @@FETCH_STATUS<>0
-	-- ���� �������� ������� �� �������, �� ������ �������� ����� �� �������
+	-- Если получить элемент не удалось, то полная резерная копия не найдена
 	BEGIN
-		SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-		SET @finalmassage = '�� ������� ������ ��������� ����� ��� ���� ������ ' + @DBName_From
+		SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+		SET @finalmassage = 'Не найдена полная резервная копия для базы данных ' + @DBName_From
 	END
 ELSE
 	BEGIN
 
-	--����� ��������� ��� ����� ��������� ����� � 3 �����:
+	--Далее загружаем все файлы резервных копий в 3 этапа:
 
-	-- 1. ��������� ������ �����
+	-- 1. Загружаем полный бэкап
 	SET @SQLString = 
 	N'RESTORE DATABASE [' + @DBName_To + ']
 	FROM DISK = N''' + @backupfile + ''' 
 	WITH  
 	FILE = 1,'
 
-	-- ����������� ����� ���� ������ �� ��������
-	-- ����� ���� �� ���� ������ ���� ������
+	-- Переименуем файлы базы данных на исходные
+	-- Новый цикл по всем файлам базы данных
 	DECLARE fnc CURSOR LOCAL FAST_FORWARD FOR 
 	(
 		SELECT
@@ -170,22 +170,22 @@ ELSE
 	REPLACE,
 	STATS = 5'
 
-	-- ������� � ��������� ���������� ����������
+	-- Выводим и выполняем полученную инструкцию
 	PRINT @SQLString
 	EXEC sp_executesql @SQLString
 	SET @error = @@error
 	IF @error <> 0
 		BEGIN
-			-- ���� ���� ������, �� ������������ ������ ����� �� �������
-			SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-			SET @finalmassage = '������ �������������� ������ ��������� ����� ��� ���� ������ ' + @DBName_To + CHAR(13) + CHAR(13)
-				+ '��� ������: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
-				+ '����� T-SQL: ' + CHAR(13) + @SQLString
+			-- Если были ошибки, то восстановить полную копию не удалось
+			SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+			SET @finalmassage = 'Ошибка восстановления полной резервной копии для базы данных ' + @DBName_To + CHAR(13) + CHAR(13)
+				+ 'Код ошибки: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
+				+ 'Текст T-SQL: ' + CHAR(13) + @SQLString
 		END
 	ELSE 
 		BEGIN
 
-		-- 2. ��������� ���������� ����� � ������� ����������
+		-- 2. Загружаем разностный бэкап и журналы транзакций
 		FETCH bkf INTO @backupfile;
 		WHILE @@FETCH_STATUS=0
 			BEGIN
@@ -197,93 +197,93 @@ ELSE
 			NORECOVERY,
 			STATS = 5'
 			
-			-- ������� � ��������� ���������� ����������
+			-- Выводим и выполняем полученную инструкцию
 			PRINT @SQLString	
 			EXEC sp_executesql @SQLString
 			SET @error = @@error
 			IF @error <> 0
 				BEGIN
-					-- ���� ������, ��������� ����
+					-- Если ошибка, прерываем цикл
 				BREAK
 				END
 			ELSE
 				BEGIN
-					-- ������ ���, ������������ ��������� ������� �����
+					-- Ошибок нет, обрабатываем следующий элемент цикла
 					FETCH bkf INTO @backupfile;
 				END
 			END;
 		
 		IF @error <> 0
 			BEGIN
-				-- �� ����� �����, ���� ������
-				SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-				SET @finalmassage = '������ �������������� ��������� ����� ��� ���� ������ ' + @DBName_To + CHAR(13) + CHAR(13)
-					+ '��� ������: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
-					+ '����� T-SQL: ' + CHAR(13) + @SQLString
+				-- Во время цикла, была ошибка
+				SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+				SET @finalmassage = 'Ошибка восстановления резервной копии для базы данных ' + @DBName_To + CHAR(13) + CHAR(13)
+					+ 'Код ошибки: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
+					+ 'Текст T-SQL: ' + CHAR(13) + @SQLString
 			END
 		ELSE
 			BEGIN
 
-			-- 3. ��������� ���� � ����������� �����
+			-- 3. Переводим базу в оперативный режим
 			SET @SQLString = 
 			N'RESTORE DATABASE ' + @DBName_To + '
 			WITH RECOVERY'
 			
-			-- ������� � ��������� ���������� ����������
+			-- Выводим и выполняем полученную инструкцию
 			PRINT @SQLString	
 			EXEC sp_executesql @SQLString
 			SET @error = @@error
 			IF @error <> 0
 				BEGIN
-					-- ������ �������� ���� � ����������� �����
-					SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-					SET @finalmassage = '������ �������� � ����������� ����� ���� ������ ' + @DBName_To + CHAR(13) + CHAR(13)
-						+ '��� ������: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
-						+ '����� T-SQL: ' + CHAR(13) + @SQLString
+					-- Ошибка перевода базы в оперативный режим
+					SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+					SET @finalmassage = 'Ошибка перевода в оперативный режим базы данных ' + @DBName_To + CHAR(13) + CHAR(13)
+						+ 'Код ошибки: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
+						+ 'Текст T-SQL: ' + CHAR(13) + @SQLString
 				END
 			ELSE	
 				BEGIN
 
-				-- ��������� ���� � ������� ������ ��������������
+				-- Переводим базу в простую модель восстановления
 				SET @SQLString = 
 					'ALTER DATABASE ' + @DBName_To + ' SET RECOVERY SIMPLE;'
 				
-				-- ������� � ��������� ���������� ����������
+				-- Выводим и выполняем полученную инструкцию
 				PRINT @SQLString	
 				EXEC sp_executesql @SQLString
 				SET @error = @@error
 				IF @error <> 0
 					BEGIN
-						-- ������ �������� ���� � ������� ������ �������������
-						SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-						SET @finalmassage = '������ �������� � ������� ������ �������������� ���� ������ ' + @DBName_To + CHAR(13) + CHAR(13)
-							+ '��� ������: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
-							+ '����� T-SQL: ' + CHAR(13) + @SQLString
+						-- Ошибка перевода базы в простую модель восстановлеия
+						SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+						SET @finalmassage = 'Ошибка перевода в простую модель восстановления базы данных ' + @DBName_To + CHAR(13) + CHAR(13)
+							+ 'Код ошибки: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
+							+ 'Текст T-SQL: ' + CHAR(13) + @SQLString
 					END
 				ELSE
 					BEGIN
 
-					-- ��������� ������ ���� ������
+					-- Запускаем сжатие базы данных
 					SET @SQLString = 
 						'DBCC SHRINKDATABASE(N''' + @DBName_To + ''');'
 					
-					-- ������� � ��������� ���������� ����������
+					-- Выводим и выполняем полученную инструкцию
 					PRINT @SQLString
 					EXEC sp_executesql @SQLString
 					SET @error = @@error
 					IF @error <> 0
 						BEGIN
-							-- ������ ������ ���� ������
-							SET @subject = '������ �������������� ���� ������ ' + @DBName_To
-							SET @finalmassage = '������ ������ ���� ������ ' + @DBName_To + CHAR(13) + CHAR(13)
-								+ '��� ������: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
-								+ '����� T-SQL: ' + CHAR(13) + @SQLString
+							-- Ошбика сжатия базы данных
+							SET @subject = 'ОШИБКА ВОССТАНОВЛЕНИЯ базы данных ' + @DBName_To
+							SET @finalmassage = 'Ошибка сжатия базы данных ' + @DBName_To + CHAR(13) + CHAR(13)
+								+ 'Код ошибки: ' + CAST(@error as NVARCHAR(10)) + CHAR(13) + CHAR(13)
+								+ 'Текст T-SQL: ' + CHAR(13) + @SQLString
 						END
 					ELSE
 						BEGIN
-							-- �������� ���������� ���� ��������
-							SET @subject = '�������� �������������� ���� ������ ' + @DBName_To
-							SET @finalmassage = '�������� �������������� ���� ������ ' + @DBName_To + ' �� ��������� ����� ���� ������ ' + @DBName_From + ' �� ������ ������� ' + Replace(CONVERT(nvarchar, @BackupTime, 126),':','-')
+							-- Успешное выполнение всех операций
+							SET @subject = 'Успешное восстановление базы данных ' + @DBName_To
+							SET @finalmassage = 'Успешное восстановление базы данных ' + @DBName_To + ' из резервной копии базы данных ' + @DBName_From + ' на момент времени ' + Replace(CONVERT(nvarchar, @BackupTime, 126),':','-')
 						END
 					END
 				END
@@ -291,17 +291,17 @@ ELSE
 		END			
 	END
 
--- ��������� ����
+-- Завершаем цикл
 CLOSE bkf;
 DEALLOCATE bkf;
 
--- ������� ��������� �������
+-- Удаляем временные таблицы
 drop table #BackupFiles
 drop table #FullBackup
 drop table #DiffBackup
 drop table #LogBackup
 
--- ���� ����� ������� ����������� �����, �������� ���������
+-- Если задан профиль электронной почты, отправим сообщение
 IF @profile_name <> ''
 EXEC msdb.dbo.sp_send_dbmail
     @profile_name = @profile_name,
@@ -309,6 +309,6 @@ EXEC msdb.dbo.sp_send_dbmail
     @body = @finalmassage,
     @subject = @subject;
 
--- ������� ��������� � ����������
+-- Выводим сообщение о резульатте
 SELECT
 	@subject as massage
