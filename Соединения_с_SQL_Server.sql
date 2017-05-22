6d769721c432170a65d639b7ecf6f77cbd4a2906
Tavalik <tavalik@mail.ru>
2017-05-22 15:19:01 +0600

    Добавил скрпит

---
+++ Соединения_с_SQL_Server.sql
@@ -0,0 +1,20 @@
+﻿-------------------------------------------
+-- Скрипт показывает текущие соединения с MS SQL-сервером
+-- Автор: Онянов Виталий (Tavalik.ru)
+-- Версия от 22.05.2017
+-- Свежие версии скриптов: https://github.com/Tavalik/SQL_TScripts
+
+-------------------------------------------
+-- ТЕЛО СКРИПТА
+SELECT 
+	program_name, 
+	net_transport 
+FROM sys.dm_exec_sessions AS sessions 
+	left join sys.dm_exec_connections AS connections 
+	ON sessions.session_id = connections.session_id 
+WHERE
+	not sessions.program_name is null
+ORDER BY
+	program_name
+	
+GO 
