88e1dd9939c25eb4901d7cffcfef10ed7e083472
Tavalik <tavalik@mail.ru>
2017-05-22 14:00:32 +0600

    Добавил скрипт

---
+++ Версия_SQL_Server.sql
@@ -0,0 +1,14 @@
+﻿-------------------------------------------
+-- Скрипт показывает версию MS SQL-сервера
+-- Автор: Онянов Виталий (Tavalik.ru)
+-- Версия от 22.05.2017
+-- Свежие версии скриптов: https://github.com/Tavalik/SQL_TScripts
+
+-------------------------------------------
+-- ТЕЛО СКРИПТА
+SELECT
+	SERVERPROPERTY('MachineName') AS ComputerName,
+	SERVERPROPERTY('ServerName') AS InstanceName,  
+	SERVERPROPERTY('Edition') AS Edition,
+	SERVERPROPERTY('ProductVersion') AS ProductVersion,  
+	SERVERPROPERTY('ProductLevel') AS ProductLevel; 
